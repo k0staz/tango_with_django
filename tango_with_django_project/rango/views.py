f4e9fe1a3966e4452f13d9889b8f2295ae0af97d
k0staz <kostazpopov@gmail.com>
2020-08-26 22:03:11 +0300

    This is progres done by following chapter 4 of Tango with Django

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says some bullshit in my opinion")
+    # Constucting a dictionary to pass it as template's context
+    context_dict = {'boldmessage': 'Some bullshit is written here'}
+
+    # Returning a render response to sent to the client
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'creatorsname': 'Konstantin Popov'}
+
+    return render(request, 'rango/about.html', context=context_dict)
