c7d1c39bc2b09d02230ae58d5245467cd782b602
k0staz <kostazpopov@gmail.com>
2020-08-28 03:32:42 +0300

    Progress made during compliting of chapter 5 "Models and Databases"

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+class PagesInLine(admin.TabularInline):
+    model = Page
+    extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Statistic', {'fields': ['views', 'likes'], 'classes': ['collapse']}),
+    ]
+    inlines = [PagesInLine]
+    list_display = ('name', 'views', 'likes')
+    list_filter = ['views', 'likes']
+    search_fields = ['name']
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
